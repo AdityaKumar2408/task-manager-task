51e771b1b73920d1dbff23aff32a4183e5481b9b
Aditya Kumar <110921916+AdityaKumar2408@users.noreply.github.com>
2024-12-15 13:34:33 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,12 +10,13 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(cors({
-  origin: 'https://admirable-treacle-99a153.netlify.app/', 
-  credentials: true, 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+// app.use(cors({
+//   origin: 'https://admirable-treacle-99a153.netlify.app/', 
+//   credentials: true, 
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+//   allowedHeaders: ['Content-Type', 'Authorization']
+// }));
+app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
