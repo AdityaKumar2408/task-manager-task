3bb8979379d68b3bef28362ee1ddbf36ea8cdcd8
Aditya Kumar <110921916+AdityaKumar2408@users.noreply.github.com>
2024-12-15 12:19:11 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,13 +11,11 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors({
-    
-    origin: true,
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+  origin: 'https://task-manager-task-4.onrender.com', 
+  credentials: true, 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
-
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
